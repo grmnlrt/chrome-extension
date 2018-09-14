78481c34c0075d6e76ef633eaa5c08fd639b1766
Germain Loret <germain.loret@gmail.com>
2018-09-14 15:36:48 +0200

    done

---
+++ background.js
@@ -0,0 +1,6 @@
+chrome.browserAction.onClicked.addListener((tab) => {
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="#C3413B"'
+  });
+});
+
---
+++ popup.js
@@ -0,0 +1,11 @@
+function listenClick() {
+const button = document.getElementById('send-data');
+button.addEventListener('click', () => {
+    chrome.tabs.executeScript({
+      file: 'scripts/send-data.js'
+    });
+  })
+}
+
+listenClick();
+
